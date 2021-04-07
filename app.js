2848876ea4c13dbd68ea46523eeeda373bd70108
7204-OMER <Eyupbhr24@gmail.com>
2021-04-08 01:18:42 +0300

    Enter Click

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let addToDoButton = document.getElementById('addToDo');
-let toDoContainer = document.getElementById('letter');
+let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 let warn = document.getElementById('h3');
-
+var input = document.getElementById('inputField');
 addToDoButton.addEventListener('click', function(){
 let paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
@@ -10,15 +10,19 @@ let paragraph = document.createElement('p');
     toDoContainer.appendChild(paragraph);
     paragraph.style.background = "white";
     inputField.value = "";
+    
     paragraph.addEventListener('click', function(){
-    paragraph.style.textDecoration = "line-through";
-})
+        paragraph.style.textDecoration = "line-through";
+    })
     paragraph.addEventListener('dblclick', function(){
-    toDoContainer.removeChild(paragraph);
-})
-})
-warn.addEventListener('mouseover', function(event){
-event.target.style.opacity = 0; 
+        toDoContainer.removeChild(paragraph);
+    })
+    inputField.addEventListener("keyup", function(event){
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("addToDo").click();
+        }
+    })
 })
 // paragraph.addEventListener('dblclick', function(){
     //     toDoContainer.appendChild(paragraph);
